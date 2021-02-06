15a74cc90ace3d4c58bad74ff08286ac32a2bb56
Hunter <hunter@line98.dev>
2021-02-05 22:05:14 -0500

    Added inital project files

---
+++ RR.java
@@ -0,0 +1,14 @@
+/*
+ *
+ * Round-robin scheduling algorithm.
+ *
+ * Add your implementation inside the RR class below.
+ *
+ */
+ 
+import java.util.*;
+
+public class RR implements Algorithm
+{
+
+}
---
+++ SJF.java
@@ -0,0 +1,12 @@
+/*
+ * SJF scheduling algorithm.
+ *
+ * Add your implementation inside the SJF class below.
+ */
+ 
+import java.util.*;
+
+public class SJF implements Algorithm
+{
+
+}
